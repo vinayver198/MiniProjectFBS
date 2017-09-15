958d7ecb85e4edfb53810f2f4267b21fa6186afe
vrnvikas <vrnvikas1994@gmail.com>
2017-09-15 18:40:06 +0530

    initial commit revised

---
+++ FeedBackManagemetSystem/src/com/cg/fms/util/DBUtil.java
@@ -0,0 +1,40 @@
+package com.cg.fms.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBUtil {
+
+	static InitialContext context;
+	static DataSource dataSource;
+	static Connection connection;
+	
+	static
+	{
+		try {
+			context = new InitialContext();
+			dataSource = (DataSource)context.lookup("java:/jdbc/TicketDB");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnect()
+	{
+		try {
+			connection = dataSource.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	
+}
